afeae27f95c635a8b66423bccd64ea28e3648615
Bezalius <adz001@ucsd.edu>
2019-04-07 11:31:19 -0700

    Added in Leo's code

--- LanguageProcessingFunctions.js
+++ LanguageProcessingFunctions.js
@@ -1,120 +1,70 @@
-/*
-var express = require('express');
-var app = express();
 
+	var Dictionary = require("oxford-dictionary");			//npm install oxford-dictionary
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+ 	var config = {
+    	app_id : "f6ea9ce4",
+    	app_key : "9698079718dfe76973308b851c6ed526",
+    	source_lang : "en"
+  	};
+	var dict = new Dictionary(config);
 
-*/
-var HashMap = require('hashmap');						//npm install hashmap
-var Dictionary = require("oxford-dictionary");			//npm install oxford-dictionary
+	var methods = {
+		//Read in Library
+		readWordData: function(wordDataFile, wordDatabase) {
 
- var config = {
-    app_id : "f6ea9ce4",
-    app_key : "9698079718dfe76973308b851c6ed526",
-    source_lang : "en"
-  };
-var dict = new Dictionary(config);
+			var fs  = require("fs");
+			var frequencies = fs.readFileSync(wordDataFile).toString().split(/\n|\r/);
+			frequencies = frequencies.filter(Boolean)
 
+			wordDatabase = wordDatabase.concat(frequencies);
 
-//Weight = num(datafile_word > WORD_FREQUENCY_THRESHOLD)/ numOfDataFiles
-var numOfDataFiles = 0;
-const WORD_FREQUENCY_THRESHOLD = 2;
-const DATA_FREQUENCY_THRESHOLD = 0.5;
+			return wordDatabase;
+		},
 
-//Read in Library
-function readWordData(wordDataFile, frequencyDatabase) {
+		//Split User input
+		splitText: function(inpText) {
+			inputWords =  inpText.match(/\b(\w+)'?(\w+)?\b/g);
+			//array2 =  text.match(/\b(\w+)\/?(\w+)'?(\w+)?\b/g); 			//works for words like I/O where theres a / in the words
 
-	var fs  = require("fs");
-	var frequencies = fs.readFileSync(wordDataFile).toString().split(/\n|\r/);
-	frequencies = frequencies.filter(Boolean)
+			for (var i = 0, L=inputWords.length ; i < L; i++) {
+			  inputWords[i]=inputWords[i].toLowerCase();
+			}
 
-	frequencies = frequencies.map((value) => value.split(/ : /)).filter(Boolean)
+			inputWords = inputWords.filter( onlyUnique );
+			return inputWords
+		},
 
-	var map = new HashMap(frequencies);
+		//Helper Function for splitText filters
+		onlyUnique: function(value, index, self) { 
+    		return self.indexOf(value) === index;
+		},
 
-	for (var i = 0; i < frequencies.length; i++) {
-		if(frequencies[i][1] > WORD_FREQUENCY_THRESHOLD) {
-			if(frequencyDatabase.has(frequencies[i][0])) {
-				frequencyDatabase.set(frequencies[i][0], frequencyDatabase.get(frequencies[i][0]) + 1);
+		isImportant: function(word, wordDatabase) {
+			if (wordDatabase.includes(word)) {
+				return true;
 			}
 			else {
-				frequencyDatabase.set(frequencies[i][0], 1);
-
+				return false;
 			}
+		},		
+
+		oxfordLookup: async function(word) {
+			var lookup = dict.find(word);
+ 			var definition;
+			await lookup.then(function(res) {
+				var temp = JSON.parse(JSON.stringify(res, null, 4))
+				definition = temp.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
+			},
+				function(err) {
+				console.log(err);
+			});
+			return definition;
 		}
 	}
-	numOfDataFiles++;
-	return frequencyDatabase;
-}
+	module.exports = methods;
 
-//Split User input
-function splitText(inpText) {
-	inputWords =  inpText.match(/\b(\w+)'?(\w+)?\b/g);
-	//array2 =  text.match(/\b(\w+)\/?(\w+)'?(\w+)?\b/g); 			//works for words like I/O where theres a / in the words
 
-	for (var i = 0, L=inputWords.length ; i < L; i++) {
-	  inputWords[i]=inputWords[i].toLowerCase();
-	}
 
-	inputWords = inputWords.filter( onlyUnique );
-	return inputWords
-}
 
-//Helper Function for splitText filters
-function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
-}
 
-function isFrequentlyUsed(word, frequencyDatabase) {
-	if (frequencyDatabase.has(word)) {
-		var weight = frequencyDatabase.get(word) / numOfDataFiles;
-		if (weight > DATA_FREQUENCY_THRESHOLD) {
-			return true;
-		}
-	}
-	return false;
-}
 
-function test (phrase, frequencyDatabase){
-	console.log("Start\n");
-	var words = splitText(phrase)
-	for (var i = 0; i < words.length; i++) {
-		console.log(isFrequentlyUsed(words[i], frequencyDatabase));
-	}
-}
-
-function oxfordLookup(word) {
-	var lookup = dict.find(word);
- 	var definition;
-	lookup.then(function(res) {
-		var temp = JSON.parse(JSON.stringify(res, null, 4))
-		definition = temp.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
-	},
-		function(err) {
-		console.log(err);
-	});
-	return definition;
-}
-
-var frequencyDatabase = new HashMap();
-
-frequencyDatabase = readWordData("Test_File.txt", frequencyDatabase);
-
-frequencyDatabase = readWordData("Test_File2.txt", frequencyDatabase);
-
-console.log(frequencyDatabase);
-
-console.log(isFrequentlyUsed("evil", frequencyDatabase));
-
-test("I hate evil people.",frequencyDatabase);
-
-let data = oxfordLookup("yes")
-data.then(function(result){
-	console.log(result)
-})
--- server.js
+++ server.js
@@ -1,13 +1,80 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var lan = require('./LanguageProcessingFunctions.js');
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+
+app.use(express.static('css'))
 
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
+   app.get('/',function(req,res) {
+		res.sendFile('newfile.html', { root: '.' })
+	});
    
    console.log("Example app listening at http://%s:%s", host, port)
 })
+
+/*
+
+var wordDatabase = new Array();
+
+wordDatabase = lan.readWordData("Test_File.txt", wordDatabase);
+
+console.log(wordDatabase);
+
+console.log(lan.isImportant("evil", wordDatabase));
+
+		
+*/
+
+var Language = require("./LanguageProcessingFunctions.js");
+var HashMap = require('hashmap');
+
+var cardSet = new HashMap();
+//var database = map<word, freq>;
+function defineInput(input) {
+	//Space separated words as our note input
+	var words = input.split(" "); 
+	words.forEach(function(element) {
+		//Lowercase and trimmed word
+		var term = element.replace(/[.,'"!?\s]/gi, '').toLowerCase();
+		//If found in the map, we do not define the input.
+		//OR if the rarity value is too high (common word)
+		var condition = isFrequentlyUsed(term, frequencyDatabase);
+		var mydiv = document.getElementById("paragraph"); 
+		mydiv.innerHTML = ""; //Reset input. For debugging.
+		var newcontent = document.createElement('div');
+		newcontent.innerHTML = element + " "; //Default if common element. Not defined.
+		if (!condition) {
+			//If the element is rarely found, then add an onmouseover
+			//that displays its definition in the corresponding text box.
+			//Highlight text with class="highlight".
+			var startStr = "<div class=\"highlight\" onmouseover=\"showDef(\'";
+			var midStr = "\')\" onmouseout=\"defaultState()\">";
+			var endStr = " </div>"; //Included space after element to restore format post-splitting
+			newcontent.innerHTML = startStr + term + midStr + term + endStr;
+			
+			let data = Lan.oxfordLookup(term);
+			data.then(function(result){
+					cardSet.set(term, result);
+			});
+
+		} 
+		if (newcontent.firstChild) {
+			mydiv.appendChild(newcontent.firstChild);
+		}
+	});
+}
+
+//Output the word and its definition to the textbox when hovered over
+function showDef(name) {
+	document.getElementById("textbox").innerHTML = name;
+	document.getElementById("answerbox").innerHTML = cardSet.get(name);
+}
+//When not hovering, return to default state (informative text box)
+function defaultState() {
+	document.getElementById("textbox").innerHTML = "Hover a highlghted word to see its definition!";
+	document.getElementById("answerbox").innerHTML = "";
+}
